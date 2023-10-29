51a70d68ca46a5c146ea0b9bf98a77e67805f2f8
Abdul Hafizh <abdulha05518@gmail.com>
2023-10-29 17:19:05 +0700

    img

--- application/views/sidebar_v.php
+++ application/views/sidebar_v.php
@@ -31,6 +31,7 @@
                 <div id="two-column-menu"></div>
                     
                 <div class="text-center">
+                    <img class="mt-2" src="<?php echo base_url()?>assets/images/logo-bkn-.png" alt="Header Avatar" height="50"> <br/>
                     <img class="rounded-circle" src="<?php echo base_url()?>assets/images/users/user-dummy-img.jpg" alt="Header Avatar" height="150">
                 </div>
                 
