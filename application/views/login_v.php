02fa6fd344e7cb4da740e08b05b14e469c7879e7
Alann Maulana <alann_it06@yahoo.co.id>
2023-11-03 08:51:35 +0700

    remove footer login

--- application/views/login_v.php
+++ application/views/login_v.php
@@ -111,24 +111,6 @@
             <!-- end container -->
         </div>
         <!-- end auth page content -->
-
-        <!-- footer -->
-        <footer class="footer start-0">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="text-center">
-                            <p class="mb-0 text-muted">&copy;
-                                <script>
-                                    document.write(new Date().getFullYear())
-                                </script> Crafted with <i class="mdi mdi-heart text-danger"></i>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <!-- end Footer -->
     </div>
     <!-- end auth-page-wrapper -->
 
