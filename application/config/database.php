2fd4763bc69878ddcf8ec07bfb462de027d9bfdc
yosef kely <yosef.kely89@gmail.com>
2023-11-12 00:48:20 +0700

    update login

--- application/config/database.php
+++ application/config/database.php
@@ -78,7 +78,7 @@ $db['default'] = array(
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'sucofindo',
+	'database' => 'admin_ssima',
 
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
